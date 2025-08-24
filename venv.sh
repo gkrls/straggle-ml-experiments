b0020476414b574a6573594c1f4a61c7368933db
gkrls <gkrls.codes@gmail.com>
2025-08-25 01:50:25 +0200

    up

--- venv.sh
+++ venv.sh
@@ -1,3 +1,5 @@
-source "$HOME/straggle-ml-experiments/bin/activate"
+cd $HOME/straggle-ml-experiments
+python3 -m venv venv
+source "$HOME/straggle-ml-experiments/venv/bin/activate"
 python -m pip install --upgrade pip
-python -m pip install --no-user -r "$REQ_FILE"
+python -m pip install --no-user -r "$HOME/straggle-ml-experiments/requirements.txt"
