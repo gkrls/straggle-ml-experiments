76aa4dd2921a459e89392bdff79fd69f37124848
gkrls <gkrls.codes@gmail.com>
2025-08-27 01:32:26 +0200

    fix resnet

--- models/vgg.py
+++ models/vgg.py
@@ -62,11 +62,6 @@ class AverageMeter:
         self.count += n
         self.avg = self.sum / max(1.0, self.count)
     def all_reduce(self):
-        # if torch.distributed.is_available() and torch.distributed.is_initialized():
-        #     t = torch.tensor([self.sum, self.count], dtype=torch.float64, device="cpu")
-        #     torch.distributed.all_reduce(t)
-        #     self.sum, self.count = t.tolist()
-        #     self.avg = self.sum / max(1.0, self.count)
         if dist.is_available() and dist.is_initialized():
             backend = dist.get_backend()
             # NCCL => must use GPU tensor; otherwise CPU is fine
@@ -100,16 +95,13 @@ def validate(model, loader, device, args):
     top1, top5, losses = AverageMeter(), AverageMeter(), AverageMeter()
     criterion = nn.CrossEntropyLoss().to(device)
 
-
     def run_validation(dataloader):
         with torch.no_grad():
             for images, targets in dataloader:
-                images = images.to(device, non_blocking=True)
-                # images = images.to(device, non_blocking=True, memory_format=torch.channels_last) if device.type == 'cuda' else images.to(device, non_blocking=True)
+                images = images.to(device, non_blocking=True, memory_format=torch.channels_last)
                 targets = targets.to(device, non_blocking=True)
                 if args.amp and device.type == 'cuda':
                     with torch.amp.autocast(device_type='cuda'):
-                    # with torch.cuda.amp.autocast(device_type='cuda'):
                         outputs = model(images)
                         loss = criterion(outputs, targets)
                 else:
@@ -136,7 +128,6 @@ def train_one_epoch(model, dataloader, criterion, optimizer, device, scaler):
     model.train()
     total_loss, samples_seen = 0.0, 0.0
     
-
     if device.type == 'cuda':
         start = torch.cuda.Event(enable_timing=True)
         end   = torch.cuda.Event(enable_timing=True)
@@ -145,14 +136,12 @@ def train_one_epoch(model, dataloader, criterion, optimizer, device, scaler):
         start = time.perf_counter()
 
     for images, targets in dataloader:
-        images = images.to(device, non_blocking=True)
-        # images = images.to(device, non_blocking=True, memory_format=torch.channels_last) if device.type == 'cuda' else images.to(device, non_blocking=True)
+        images = images.to(device, non_blocking=True, memory_format=torch.channels_last)
         targets = targets.to(device, non_blocking=True)
         samples_seen += images.size(0)
 
         optimizer.zero_grad(set_to_none=True)
         if scaler is not None:
-            # with torch.cuda.amp.autocast():
             with torch.amp.autocast(device_type='cuda'):
                 outputs = model(images)
                 loss = criterion(outputs, targets)
@@ -194,28 +183,19 @@ def train(args):
 
     # Model
     model = None
-    if args.model == 'resnet50':
-        model = models.resnet50(num_classes=args.num_classes).to(device)
-    elif args.model == 'resnet101':
-        model = models.resnet101(num_classes=args.num_classes).to(device)
+    if args.model == 'vgg11':
+        model = models.vgg11(num_classes=args.num_classes).to(device, memory_format=torch.channels_last)
+    elif args.model == 'vgg19':
+        model = models.vgg19(num_classes=args.num_classes).to(device, memory_format=torch.channels_last)
     else:
-        model = models.resnet152(num_classes=args.num_classes).to(device)
+        raise ValueError(f"Unsupported model: {args.model}")
 
-    model = DDP(model, device_ids=[args.local_rank] if device.type == "cuda" else None, gradient_as_bucket_view=True, \
-                find_unused_parameters=False, static_graph=args.static_graph)
-    # model = models.resnet50(num_classes=args.num_classes)
-    # if device.type == "cuda":
-    #     model = model.to(device, memory_format=torch.channels_last)
-    #     model = DDP(model, device_ids=[args.local_rank])
-    # else:
-    #     model = model.to(device)  # keep default NCHW on CPU
-    #     model = DDP(model)
-
+    model = DDP(model, device_ids=[args.local_rank] if device.type == "cuda" else None, gradient_as_bucket_view=True, static_graph=args.static_graph)
 
     print(f"Model '{args.model}' initialized.", flush=True)
 
     criterion = nn.CrossEntropyLoss().to(device)
-    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum,  weight_decay=args.weight_decay, fused=True) # foreach=True, 
+    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum,  weight_decay=args.weight_decay, fused=True)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
     scaler = torch.amp.GradScaler('cuda', enabled=args.amp) if device.type == "cuda" else None
 
@@ -227,11 +207,6 @@ def train(args):
     if args.rank == 0:
         log = {"time": now(), "config": vars(args), "epochs": {}}
         save_log(args.json, log)
-
-
-    # print("len(train classes) =", len(train_dataset.classes))
-    # print("len(val classes)   =", len(val_dataset.classes))
-    # assert train_dataset.classes == val_dataset.classes, "train/val class folder names differ"
     
     for epoch in range(args.epochs):
         print(f"[{now()}][Epoch {epoch:03d}] ...")
@@ -269,12 +244,6 @@ def train(args):
         if args.rank == 0 and top1 > best_top1: best_top1 = top1
         if args.rank == 0 and top5 > best_top5: best_top5 = top5
 
-
-    # if args.rank == 0:
-        # with open(args.json, "w") as f:
-        #     import json
-        #     json.dump(log, f, indent=2)
-
 # ------------------------- Entry / Setup ------------------------
 
 def setup_ddp(args):
@@ -297,23 +266,19 @@ def setup_ddp(args):
     os.environ.setdefault("MASTER_ADDR", args.master_addr)
     os.environ.setdefault("MASTER_PORT", str(args.master_port))
     os.environ.setdefault("GLOO_SOCKET_IFNAME", args.iface)
-    # os.environ.setdefault("NCCL_SOCKET_IFNAME", args.iface)
+    os.environ.setdefault("NCCL_SOCKET_IFNAME", args.iface)
     os.environ.setdefault("LOCAL_RANK",  str(args.local_rank))
 
     os.environ.setdefault("NCCL_SOCKET_IFNAME", args.iface)               # e.g. ens4f0
-    os.environ.setdefault("NCCL_DEBUG", "WARN")
+    os.environ.setdefault("NCCL_DEBUG", "INFO")
     os.environ.setdefault("NCCL_DEBUG_SUBSYS", "INIT,NET,ENV")
     os.environ.setdefault("NCCL_DEBUG_FILE", f"/tmp/nccl_%h_rank{os.environ.get('RANK','0')}.log")
-    os.environ.setdefault("NCCL_P2P_DISABLE", "1")         # P100 P2P is limited
-    os.environ.setdefault("NCCL_TREE_THRESHOLD", "0")      # Force ring for stability
-    os.environ.setdefault("NCCL_IB_DISABLE", "0")          # Enable IB if available on 100G
-    os.environ.setdefault("NCCL_BUFFSIZE", "4194304")      # 4MB buffers
-    os.environ.setdefault("NCCL_SOCKET_NTHREADS", "4")  # More NCCL threads
+    os.environ.setdefault("NCCL_SOCKET_NTHREADS", "8")  # More NCCL threads
     os.environ.setdefault("NCCL_NSOCKS_PERTHREAD", "4")
+
     # Start the process group
     dist.init_process_group(backend=args.backend, init_method="env://", rank=args.rank, world_size=args.world_size, timeout=datetime.timedelta(seconds=30))
 
-    # if torch.cuda.is_available(): torch.cuda.set_device(args.local_rank)
     if args.rank == 0:
         print(f"[DDP] backend={args.backend} world_size={args.world_size} "
               f"master={args.master_addr}:{args.master_port} iface={args.iface} local_rank={args.local_rank}", flush=True)
@@ -335,7 +300,7 @@ def main():
     parser.add_argument("--deterministic", action='store_true')
     parser.add_argument("--workers", type=int, default=4)
 
-    parser.add_argument('--model', type=str, choices=['resnet50', 'resnet101', 'resnet152'], help="Resnet model", default="resnet50")
+    parser.add_argument('--model', type=str, choices=['vgg11', 'vgg19'], help="VGG model", default="vgg11")
     parser.add_argument('--data', type=str, required=True)
     parser.add_argument('--epochs', type=int, default=90)
     parser.add_argument('--batch_size', type=int, default=32)
@@ -348,10 +313,8 @@ def main():
     parser.add_argument("--static_graph", action='store_true', help="Enable static_graph in DDP")
     parser.add_argument("--prefetch_factor", type=int, default=2)
     
-    parser.add_argument("--json", type=str, default="resnet50.json", help="Path to JSON run log")
+    parser.add_argument("--json", type=str, default="vgg11.json", help="Path to JSON run log")
     args = parser.parse_args()
-
-
 
     if args.deterministic:
         os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
@@ -372,7 +335,6 @@ def main():
         if args.rank == 0: print("[Info] Workers requested < 1; using workers=1", flush=True)
         args.workers = 1 
 
-
     sys.stdout.reconfigure(line_buffering=True)
 
     setup_ddp(args)
@@ -382,7 +344,6 @@ def main():
         torch.manual_seed(args.seed)
         random.seed(args.seed)
         np.random.seed(args.seed)
-
 
     print(json.dumps(vars(args), indent=2))
     try:
