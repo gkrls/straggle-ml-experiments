f3f1236948afd78368132321105885b57c5aa395
gkrls <gkrls.codes@gmail.com>
2025-09-02 13:40:24 +0200

    openwebtext

--- datasets/openwebtext-prepare.sh
+++ datasets/openwebtext-prepare.sh
@@ -6,7 +6,7 @@ git -C "$HOME/straggle-ml-experiments" pull --ff-only || true
 # DIR="${1:-$HOME/datasets/imagenet}"
 # python3 $HOME/straggle-ml-experiments/datasets/imagenet-1k-prepare.py $DIR
 
-python prep_openwebtext_to_txt.py \
+python $HOME/straggle-ml-experiments/datasets/openwebtext-preparep-txt.py \
   --arc_dir "$HOME/datasets/openwebtext/openwebtext" \
   --out_dir "$HOME/datasets/openwebtext/openwebtext-txt" \
   --shard_size_mb 1024
