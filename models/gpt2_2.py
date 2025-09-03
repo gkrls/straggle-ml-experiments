c86be5792a09545ecad673e7687d00b8ce778f0e
gkrls <gkrls.codes@gmail.com>
2025-09-03 10:26:36 +0200

    gpt2

--- models/gpt2_2.py
+++ models/gpt2_2.py
@@ -232,10 +232,11 @@ def train_one_epoch(model, dataloader, optimizer, device, scaler, args,
         with sync_context:
             with torch.amp.autocast(device_type='cuda', enabled=args.amp):
                 attn = torch.ones_like(x, dtype=torch.long)  # no padding in our windows; all tokens attend
-                logits = model(x, attention_mask=attn).logits
+            logits = model(x, attention_mask=attn).logits
                 B, T, V = logits.shape
-                loss = F.cross_entropy(logits.view(B*T, V), y.view(B*T))
-                loss = loss / max(1, args.gradient_accumulation_steps)
+                loss_full = F.cross_entropy(logits.view(B*T, V), y.view(B*T))
+                loss = loss_full / max(1, args.gradient_accumulation_steps)
+                losses.update(loss_full.item(), B*T)
 
             if scaler is not None:
                 scaler.scale(loss).backward()
