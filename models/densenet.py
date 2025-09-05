3ae2b51e49223c9c399ccdf92610ce49f282068a
gkrls <gkrls.codes@gmail.com>
2025-09-05 12:50:05 +0200

    densenet stragglesim

--- models/densenet.py
+++ models/densenet.py
@@ -418,7 +418,7 @@ def main():
         if not s: return []
         try: return [int(x) for x in re.split(r"\s*,\s*", s) if x]
         except ValueError: raise argparse.ArgumentTypeError("Expected a comma-separated list of integers (e.g. 1,2,3)")
-    parser.add_argument("--straggle_points", action="store_true", help="Number of straggle points (1-3). Use 0 for no straggle sim", default=0)
+    parser.add_argument("--straggle_points", type=int, help="Number of straggle points (1-3). Use 0 for no straggle sim", default=0)
     parser.add_argument("--straggle_prob", type=float, help="Probability to straggle at each point", default=0)
     parser.add_argument("--straggle_ranks", type=csv_ints, help="comma separated list of ints", default=[])
     parser.add_argument("--straggle_amount", type=float, help="base straggle amount in seconds (e.g. mean step time)", default=10)
