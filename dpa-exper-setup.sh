3042d60dd0f9ca5b8e8c09ac3665e8660f8b8909
gkrls <gkrls.codes@gmail.com>
2025-09-21 00:21:33 +0200

    experiments

--- dpa-exper-setup.sh
+++ dpa-exper-setup.sh
@@ -5,9 +5,14 @@ else
   git -C "$HOME/straggle-ml-experiments" pull --ff-only || true
 fi
 
+# cd $HOME/straggle-ml-experiments
+# python3 -m venv venv
+# source "$HOME/straggle-ml-experiments/venv/bin/activate"
 
 cd $HOME/straggle-ml-experiments
-python3 -m venv venv
+if [ ! -d "venv" ]; then
+  python -m venv venv
+fi
 source "$HOME/straggle-ml-experiments/venv/bin/activate"
 python -m pip install --upgrade pip
 python -m pip install --no-user -r "$HOME/straggle-ml-experiments/requirements.txt"
