9b7e74e56efc7c7c5d360632680ce3b5b6992090
gkrls <gkrls.codes@gmail.com>
2025-09-21 00:24:18 +0200

    experiments

--- dpa-setup-exp.sh
+++ dpa-setup-exp.sh
@@ -16,3 +16,14 @@ fi
 source "$HOME/straggle-ml-experiments/venv/bin/activate"
 python -m pip install --upgrade pip
 python -m pip install --no-user -r "$HOME/straggle-ml-experiments/requirements.txt"
+
+cd $HOME/straggle-ml
+git reset --hard && git pull
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DDPA_DEVELOP=OFF -DDPA_AVX=ON -DDPA_AVX_512=ON -DDPA_SWITCH=OFF ..
+make -j4 install
+
+export PKG_CONFIG_PATH=/opt/mellanox/dpdk/lib/x86_64-linux-gnu/pkgconfig
+
+python $HOME/straggle-ml/build/install/lib/dpa_plugin_pytorch/setup.py develop
