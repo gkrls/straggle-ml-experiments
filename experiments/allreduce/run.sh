cc185c79004832effd2310048d57828f3766fd14
gkrls <gkrls.codes@gmail.com>
2025-09-22 01:06:40 +0200

    up

--- experiments/allreduce/run.sh
+++ experiments/allreduce/run.sh
@@ -46,10 +46,10 @@ MASTER_PORT=29500
 
 PROG=experiments/allreduce/allreduce-benchmark.py
 CONF=experiments/allreduce/edgecore.json
-VALGRIND=valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes
+VALGRIND=valgrind #--leak-check=full --show-leak-kinds=all --track-origins=yes"
 
 sudo -E $(which python) $PROG --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
-  --dpa_conf $CONF --dpa_pipes 4 -b dpa_sock -d cuda -t float32 -s 10000 -w 1 -i 1 -v
+  --dpa_conf $CONF --dpa_pipes 4 -b dpa_sock -d cuda -t float32 -s 125000000 -w 3 -i 16 -v --batch
 #"$@"
 
 # sudo -E $(which python) experiments/allreduce-benchmark.py --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
