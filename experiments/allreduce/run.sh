a94897f8dae6f2adc9d02be52c0bca5047206c4f
gkrls <gkrls.codes@gmail.com>
2025-09-24 17:10:13 +0200

    wip

--- experiments/allreduce/run.sh
+++ experiments/allreduce/run.sh
@@ -50,7 +50,7 @@ VALGRIND=valgrind #--leak-check=full --show-leak-kinds=all --track-origins=yes"
 
 sudo -E $(which python) $PROG --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
   --dpa_conf $CONF --dpa_pipes 4 -b dpa_dpdk -d cuda -t int32 -s 100000 -w 1 -i 20 \
-  --gloo_socket_ifname=$IFACE --global_stats #--batch
+  --gloo_socket_ifname=$IFACE --global_stats --batch
 #"$@"
 
 # sudo -E $(which python) experiments/allreduce-benchmark.py --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
