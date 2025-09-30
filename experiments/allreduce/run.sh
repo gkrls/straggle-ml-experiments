2e9134a1039f41dfe3d2cdb2b8e3849055292511
gkrls <gkrls.codes@gmail.com>
2025-09-30 12:41:54 +0200

    wip

--- experiments/allreduce/run.sh
+++ experiments/allreduce/run.sh
@@ -8,7 +8,7 @@ if [[ $# -eq 1 && "$1" == "sync" ]]; then
   [[ -d "$HOME/straggle-ml/.git" ]] || git clone https://github.com/gkrls/straggle-ml.git "$HOME/straggle-ml"
 
   git -C "$HOME/straggle-ml" fetch -q origin || true
-  git -C "$HOME/straggle-ml" checkout "$BRANCH" 2>/dev/null || git -C "$DIR" checkout -b "$BRANCH" --track "origin/$BRANCH" || true
+  git -C "$HOME/straggle-ml" checkout "$BRANCH" 2>/dev/null || true
   git -C "$HOME/straggle-ml" reset --hard origin/"$BRANCH" 2>/dev/null || git -C "$DIR" reset --hard || true
   git -C "$HOME/straggle-ml" pull --ff-only origin "$BRANCH" 2>/dev/null || true
   git -C "$HOME/straggle-ml" clean -ffd || true
