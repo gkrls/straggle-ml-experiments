9b8bedc64ba4a5a51d6125bbc869b32c19535a4f
gkrls <gkrls.codes@gmail.com>
2025-10-06 18:04:39 +0200

    wip

--- experiments/allreduce/run.sh
+++ experiments/allreduce/run.sh
@@ -62,7 +62,7 @@ VALGRIND=valgrind #--leak-check=full --show-leak-kinds=all --track-origins=yes"
 PROF="nsys profile -o myprofile -t cuda,osrt --stats=true --force-overwrite=true"
 
 sudo -E $(which python) $PROG --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
-  --dpa_conf $CONF --dpa_pipes 4 -b dpa_dpdk -d cuda -t float32 -s 50000000 -w 0 -i 1 \
+  --dpa_conf $CONF --dpa_pipes 4 -b dpa_dpdk -d cuda -t float32 -s 50000000 -w 5 -i 50 \
   --gloo_socket_ifname=$IFACE --global_stats --batch
 
 # sudo -E $(which python) experiments/allreduce-benchmark.py --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
