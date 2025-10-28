ffa42bd8247bcfcf666dfcba3718eea0b3ba574d
gkrls <gkrls.codes@gmail.com>
2025-10-28 14:55:43 +0100

    wip

--- experiments/allreduce/run2.sh
+++ experiments/allreduce/run2.sh
@@ -32,7 +32,7 @@ if [[ $# -eq 1 && "$1" == "sync" ]]; then
   mkdir -p $HOME/straggle-ml/build
   cd $HOME/straggle-ml/build
   cmake -DCMAKE_INSTALL_MESSAGE=LAZY \
-        -DCMAKE_BUILD_TYPE=Debug \
+        -DCMAKE_BUILD_TYPE=Release \
         -DDPA_TRACE=OFF \
         -DDPA_DEVELOP=OFF \
         -DDPA_SWITCH=OFF \
@@ -83,7 +83,9 @@ PERF="perf stat -d --"
 
 sudo -E $(which python) $PROG --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
   --dpa_conf $CONF --dpa_pipes 4 -b dpa_dpdk -d cuda -t float32 -s 25000000 -w 10 -i 100 --dpa_avg \
-  --gloo_socket_ifname=$IFACE --global_stats --pattern 3 --straggle_k 5 --straggle_rank 0 --straggle_ms 10000 --straggle_num 2 #--batch #--verify
+  --gloo_socket_ifname=$IFACE --global_stats --pattern 3 --straggle_k 6 --straggle_rank 0 --batch
+  #--straggle_ms 10000 --straggle_num 2
+  # --batch #--verify
 
 # sudo -E $(which python) experiments/allreduce-benchmark.py --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
 #   --d_conf configs/config-edgecore.json -b nccl -d cuda -t float32 -s 1000 -i 5 -w 3 -v "$@"
