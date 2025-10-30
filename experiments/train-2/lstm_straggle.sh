ae693ab2ea5a3ad546ab8da3ef7fab3a1ad2eb14
gkrls <gkrls.codes@gmail.com>
2025-10-30 16:39:58 +0100

    wip

--- experiments/train-2/lstm_straggle.sh
+++ experiments/train-2/lstm_straggle.sh
@@ -106,12 +106,11 @@ sudo -E $(which python) experiments/train-2/lstm-2.py \
   --deterministic \
   --epochs 12 \
   --batch_size 64 \
-  --straggle_points 1 \
+  --straggle_points 3 \
   --straggle_prob 16 \
   --straggle_ranks 1 \
   --straggle_amount 0.05 \
-  --straggle_multiply 0.5 2 \
-  --straggle_k 6
+  --straggle_multiply 0.5 2
   # --straggle_verbose \
   # --straggle_k $WORLD_K \
   "$@"
