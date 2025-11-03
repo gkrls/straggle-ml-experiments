02670fb06e812ef8bafe74e9fe04f6008988dad0
gkrls <gkrls.codes@gmail.com>
2025-11-03 18:36:28 +0100

    wip

--- experiments/allreduce/timeouts/plot_time_strag_on_off.py
+++ experiments/allreduce/timeouts/plot_time_strag_on_off.py
@@ -2,16 +2,17 @@ import json, os
 import matplotlib.pyplot as plt
 
 
+RANK=0
 
 data = {
   '500' : {
-    'baseline-ns' : {
-      0 : json.load(open(os.path.join(os.path.dirname(__file__), "baseline.t-500.rank-0.json"))),
-      1 : json.load(open(os.path.join(os.path.dirname(__file__), "baseline.t-500.rank-1.json")))
+    'ns' : {
+      0 : json.load(open(os.path.join(os.path.dirname(__file__), "ns.t-500.rank-0.json"))),
+      1 : json.load(open(os.path.join(os.path.dirname(__file__), "ns.t-500.rank-1.json")))
     },
-    'baseline-su' : {
-      0 : json.load(open(os.path.join(os.path.dirname(__file__), "baseline.straggle-10-10-2000.t-500-1500.rank-0.json"))),
-      1 : json.load(open(os.path.join(os.path.dirname(__file__), "baseline.straggle-10-10-2000.t-500-1500.rank-1.json"))),
+    'su' : {
+      0 : json.load(open(os.path.join(os.path.dirname(__file__), "su.straggle-10-10-2000.t-500-1500.rank-0.json"))),
+      1 : json.load(open(os.path.join(os.path.dirname(__file__), "su.straggle-10-10-2000.t-500-1500.rank-1.json"))),
     },
     "sa-3000" : {
       0 : json.load(open(os.path.join(os.path.dirname(__file__), "sa.straggle-10-10-2000.t-500-3000.rank-0.json"))),
@@ -33,7 +34,17 @@ data = {
       0 : json.load(open(os.path.join(os.path.dirname(__file__), "sa.straggle-10-10-2000.t-500-0500.rank-0.json"))),
       1 : json.load(open(os.path.join(os.path.dirname(__file__), "sa.straggle-10-10-2000.t-500-0500.rank-1.json"))),
     }
-  }
+  },
+  "1000" : {
+    "su" : {
+      0 : json.load(open(os.path.join(os.path.dirname(__file__), "su.straggle-10-10-2000.t-1000-1500.rank-0.json"))),
+      1 : json.load(open(os.path.join(os.path.dirname(__file__), "su.straggle-10-10-2000.t-1000-1500.rank-1.json"))),
+    },
+    "sa-1500" : {
+      0 : json.load(open(os.path.join(os.path.dirname(__file__), "sa.straggle-10-10-2000.t-1000-1500.rank-0.json"))),
+      1 : json.load(open(os.path.join(os.path.dirname(__file__), "sa.straggle-10-10-2000.t-1000-1500.rank-1.json"))),
+    }
+  },
 }
 
 
@@ -43,43 +54,36 @@ axes = axes.flatten()
 
 xmax = 100
 
-ns_0 = data['500']['baseline-ns'][0]['data']['times'][:xmax]
-su_0 = data['500']['baseline-su'][0]['data']['times'][:xmax]
-sa_3000_0 = data['500']['sa-3000'][0]['data']['times'][:xmax]
-sa_1500_0 = data['500']['sa-1500'][0]['data']['times'][:xmax]
-sa_1000_0 = data['500']['sa-1000'][0]['data']['times'][:xmax]
-sa_0750_0 = data['500']['sa-0750'][0]['data']['times'][:xmax]
-sa_0500_0 = data['500']['sa-0500'][0]['data']['times'][:xmax]
+ns = data['500']['ns'][RANK]['data']['times'][:xmax]
+su = data['500']['su'][RANK]['data']['times'][:xmax]
+sa_3000 = data['500']['sa-3000'][RANK]['data']['times'][:xmax]
+sa_1500 = data['500']['sa-1500'][RANK]['data']['times'][:xmax]
+sa_1000 = data['500']['sa-1000'][RANK]['data']['times'][:xmax]
+sa_0750 = data['500']['sa-0750'][RANK]['data']['times'][:xmax]
+sa_0500 = data['500']['sa-0500'][RANK]['data']['times'][:xmax]
 
-axes[0].plot(range(xmax), ns_0, 'd-', color='tab:green', linewidth=2, markersize=4, label="baseline-ns")
-axes[0].plot(range(xmax), su_0, 'd-', color='tab:red', linewidth=2, markersize=4, label="baseline-su")
-axes[0].plot(range(xmax), sa_3000_0, 's-', color='midnightblue', linewidth=2, markersize=4, label="0.5/3.0 ms")
-axes[0].plot(range(xmax), sa_1500_0, 's-', color='blue', linewidth=2, markersize=4, label="0.5/1.5 ms")
-axes[0].plot(range(xmax), sa_1000_0, 's-', color='tab:blue', linewidth=2, markersize=4, label="0.5/1.0 ms")
-axes[0].plot(range(xmax), sa_0750_0, 's-', color='dodgerblue', linewidth=2, markersize=4, label="0.5/0.75 ms")
-axes[0].plot(range(xmax), sa_0500_0, 's-', color='deepskyblue', linewidth=2, markersize=4, label="0.5/0.5 ms")
+axes[0].plot(range(xmax), ns, 'd-', color='tab:green', linewidth=2, markersize=4, label="baseline-ns")
+axes[0].plot(range(xmax), su, 'd-', color='tab:red', linewidth=2, markersize=4, label="baseline-su")
+axes[0].plot(range(xmax), sa_3000, 's-', color='midnightblue', linewidth=2, markersize=4, label="0.5/3.0 ms")
+axes[0].plot(range(xmax), sa_1500, 's-', color='blue', linewidth=2, markersize=4, label="0.5/1.5 ms")
+axes[0].plot(range(xmax), sa_1000, 's-', color='tab:blue', linewidth=2, markersize=4, label="0.5/1.0 ms")
+axes[0].plot(range(xmax), sa_0750, 's-', color='dodgerblue', linewidth=2, markersize=4, label="0.5/0.75 ms")
+axes[0].plot(range(xmax), sa_0500, 's-', color='deepskyblue', linewidth=2, markersize=4, label="0.5/0.5 ms")
 
 # axes[0].plot(list(range(len(times_lt_1))), times_lt_1, 'd-', color='tab:red', linewidth=2, markersize=4, label="straggler")
 # axes[2].plot(list(range(len(times_lb_0))), times_lb_0, 's-', color='tab:blue', linewidth=2, markersize=4, label="nostraggler")
 # axes[2].plot(list(range(len(times_lb_1))), times_lb_1, 'd-', color='tab:red', linewidth=2, markersize=4, label="straggler")
 # # right column
 
-ns_1 = data['500']['baseline-ns'][1]['data']['times'][:xmax]
-su_1 = data['500']['baseline-su'][1]['data']['times'][:xmax]
-sa_3000_1 = data['500']['sa-3000'][1]['data']['times'][:xmax]
-sa_1500_1 = data['500']['sa-1500'][1]['data']['times'][:xmax]
-sa_1000_1 = data['500']['sa-1000'][1]['data']['times'][:xmax]
-sa_0750_1 = data['500']['sa-0750'][1]['data']['times'][:xmax]
-sa_0500_1 = data['500']['sa-0500'][1]['data']['times'][:xmax]
+# ns_1 = data['500']['baseline-ns'][1]['data']['times'][:xmax]
+su           = data['500']['su'][RANK]['data']['times'][:xmax]
+sa_0500_1500 = data['500']['sa-1500'][RANK]['data']['times'][:xmax]
+sa_1000_1500 = data['1000']['sa-1500'][RANK]['data']['times'][:xmax]
 
+axes[1].plot(range(xmax), su, 'd-', color='tab:red', linewidth=2, markersize=4, label="baseline-su")
+axes[1].plot(range(xmax), sa_0500_1500, 's-', color='midnightblue', linewidth=2, markersize=4, label="0.5/1.5 ms")
+axes[1].plot(range(xmax), sa_1000_1500, 's-', color='blue', linewidth=2, markersize=4, label="1.0/1.5 ms")
 
-axes[1].plot(range(xmax), ns_1, 'd-', color='tab:green', linewidth=2, markersize=4, label="baseline-ns")
-axes[1].plot(range(xmax), su_1, 'd-', color='tab:red', linewidth=2, markersize=4, label="baseline-su")
-axes[1].plot(range(xmax), sa_3000_1, 's-', color='midnightblue', linewidth=2, markersize=4, label="0.5/3.0 ms")
-axes[1].plot(range(xmax), sa_1500_1, 's-', color='blue', linewidth=2, markersize=4, label="0.5/1.5 ms")
-axes[1].plot(range(xmax), sa_1000_1, 's-', color='tab:blue', linewidth=2, markersize=4, label="0.5/1.0 ms")
-axes[1].plot(range(xmax), sa_0750_1, 's-', color='dodgerblue', linewidth=2, markersize=4, label="0.5/0.75 ms")
-axes[1].plot(range(xmax), sa_0500_1, 's-', color='deepskyblue', linewidth=2, markersize=4, label="0.5/0.5 ms")
 
 
 # axes[1].plot(list(range(len(times_rt_1))), times_rt_1, 'd-', color='tab:red', linewidth=2, markersize=4, label="straggler")
