a13ed06e650d09f477edbe0d144f75184eeef55f
gkrls <gkrls.codes@gmail.com>
2025-11-21 13:47:39 +0100

    wip

--- experiments/allreduce/run.sh
+++ experiments/allreduce/run.sh
@@ -82,8 +82,8 @@ PERF="perf stat -d --"
 # export ASAN_OPTIONS=symbolize=1,abort_on_error=1,print_stats=1,check_initialization_order=1
 
 sudo -E $(which python) $PROG --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
-  --dpa_conf $CONF --dpa_pipes 1 -b dpa_dpdk -d cuda -t float32 -s 25000000 -w 10 -i 100 --dpa_avg --straggle_k 5 \
-  --gloo_socket_ifname=$IFACE --global_stats --straggle_rank 1 --straggle_ms 2000 --straggle_num 10 --straggle_start 10
+  --dpa_conf $CONF --dpa_pipes 4 -b dpa_dpdk -d cuda -t float32 -s 25000000 -w 10 -i 100 --dpa_avg --straggle_k 6 --batch
+  # --gloo_socket_ifname=$IFACE --global_stats --straggle_rank 1 --straggle_ms 2000 --straggle_num 10 --straggle_start 10
   # --batch #--verify
 
 # sudo -E $(which python) experiments/allreduce-benchmark.py --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
