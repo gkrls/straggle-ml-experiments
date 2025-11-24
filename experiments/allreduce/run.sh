acca91c989057fea3f298ddc00860fc66871f433
gkrls <gkrls.codes@gmail.com>
2025-11-24 16:13:58 +0100

    wip

--- experiments/allreduce/run.sh
+++ experiments/allreduce/run.sh
@@ -41,6 +41,7 @@ if [[ $# -eq 1 && "$1" == "sync" ]]; then
         -DDPA_PROFILE_FIRST=OFF \
         -DDPA_FASTESTK_EXIT=OFF \
         -DDPA_FASTESTK_BULK=ON \
+        -DDPA_SYNCHRON_BULK=ON \
         -DDPA_DPDK_RE_DISABLE=OFF \
         -DDPA_DPDK_RX_REUSE=ON \
         -DDPA_DPDK_WIN_HUGE=ON \
@@ -78,13 +79,14 @@ CONF=configs/edgecore.json
 VALGRIND=valgrind #--leak-check=full --show-leak-kinds=all --track-origins=yes"
 NSYS="nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas --cuda-memory-usage=true --sampling-period=200000 -d 30 -o $HOME/nsys_profile -f true"
 PERF="perf stat -d --"
+GDB="gdb --args"
 
 # export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libasan.so.8  # or .so.6 depending on your GCC version
 # export ASAN_OPTIONS=symbolize=1,abort_on_error=1,print_stats=1,check_initialization_order=1
 
 sudo -E $(which python) $PROG --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
   --dpa_conf $CONF --dpa_pipes 1 -b dpa_dpdk -d cuda -t float32 -s 25000000 -w 10 -i 100 --dpa_avg --straggle_k 5 \
-  --gloo_socket_ifname=$IFACE --global_stats --straggle_rank 1 --straggle_ms 2000 --straggle_num 10 --straggle_start 10
+  --gloo_socket_ifname=$IFACE --global_stats --straggle_rank 1 --straggle_ms 2000 --straggle_num 10 --straggle_start 10 --batch
   # --batch #--verify
 
 # sudo -E $(which python) experiments/allreduce-benchmark.py --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
