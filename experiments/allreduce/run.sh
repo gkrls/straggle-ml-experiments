5f9b5a5ee16a183d73dcd888b912419cf4c41e8b
gkrls <gkrls.codes@gmail.com>
2025-12-04 14:22:14 +0100

    wip

--- experiments/allreduce/run.sh
+++ experiments/allreduce/run.sh
@@ -39,7 +39,7 @@ if [[ $# -eq 1 && "$1" == "sync" ]]; then
         -DDPA_AVX=ON \
         -DDPA_PROFILE=OFF \
         -DDPA_PROFILE_FIRST=OFF \
-        -DDPA_FASTESTK_EXIT=OFF \
+        -DDPA_FASTESTK_EXIT=ON \
         -DDPA_FASTESTK_BULK=ON \
         -DDPA_SYNCHRON_BULK=OFF \
         -DDPA_DPDK_RE_DISABLE=OFF \
@@ -85,8 +85,8 @@ GDB="gdb --args"
 # export ASAN_OPTIONS=symbolize=1,abort_on_error=1,print_stats=1,check_initialization_order=1
 
 sudo -E $(which python) $PROG --rank $RANK --world_size $WORLD --master_addr $MASTER_ADDR --master_port $MASTER_PORT \
-  --dpa_conf $CONF --dpa_pipes 4 -b dpa_dpdk -d cuda -t float32 -s 25000000 -w 10 -i 100 --straggle_k 5 --dpa_avg \
-  --straggle_rank 1 --straggle_ms 2000 --straggle_num 10 --straggle_start 10 --pattern 2
+  --dpa_conf $CONF --dpa_pipes 4 -b dpa_dpdk -d cuda -t float32 -s 25000000 -w 0 -i 100 --straggle_k 5 --dpa_avg \
+  --straggle_rank 1 --straggle_ms 2000 --straggle_num 10 --straggle_start 0 --pattern 2
   # --gloo_socket_ifname $IFACE --gloo_num_threads 2
   # --nccl_socket_nthreads 6 --nccl_nsocks_perthread 2
   # --pattern 1 --nccl_ib_qps_per_connection 1
