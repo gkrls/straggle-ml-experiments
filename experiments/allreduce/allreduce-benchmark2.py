d78d5c1a8cf4bee469c1dce2727c90a892fbf6bd
gkrls <gkrls.codes@gmail.com>
2025-12-12 13:11:12 +0100

    wip

--- experiments/allreduce/allreduce-benchmark2.py
+++ experiments/allreduce/allreduce-benchmark2.py
@@ -167,8 +167,8 @@ def benchmark(args):
             # Warmup
             for i in range(args.warmup): jobs.append(dist.all_reduce(tensors[i], op=op, async_op=True))
             for j in jobs: j.wait()
-            jobs.clear()
             torch.cuda.synchronize()
+            jobs.clear()
 
             # Timed iterations - use same timing method for both CPU and CUDA
             t_start = time.time_ns()
